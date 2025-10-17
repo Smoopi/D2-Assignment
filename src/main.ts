0213b460f16424e2aa8f49bcba9bc8c2847a5de5
Smoopi <74732940+Smoopi@users.noreply.github.com>
2025-10-17 18:43:41 +0000

    Added the canvas and the title for the assignment, The canvas doesn't work yet. Step 1 done.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+document.body.innerHTML = "";
+
+const app = document.createElement("div");
+app.id = "app";
+
+const title = document.createElement("h1");
+title.textContent = "D2 - Canvas";
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.className = "workpad";
+
+app.appendChild(title);
+app.appendChild(canvas);
+document.body.appendChild(app);
